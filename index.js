8729aeb866c3cb78bba4d119b53373e566018b19
Jamison Dance <jergason@gmail.com>
2013-11-08 17:34:49 -0700

    holy crap, did i used to like that syntax?

--- index.js
+++ index.js
@@ -1,55 +1,45 @@
-(function () {
-  "use strict";
-  var fs = require('fs')
-    ;
+var fs = require('fs')
 
+// how to know when you are done?
+function readdir(path, callback) {
+  var list = []
 
-  // how to know when you are done?
-  function readdir(path, callback) {
-    var list = []
-      ;
+  fs.readdir(path, function (err, files) {
+    if (err) {
+      return callback(err)
+    }
 
+    var pending = files.length
+    if (!pending) {
+      // we are done, woop woop
+      return callback(null, list)
+    }
 
-    fs.readdir(path, function (err, files) {
-      var pending
-        ;
+    files.forEach(function (file) {
+      fs.stat(path + '/' + file, function (err, stats) {
+        if (err) {
+          return callback(err)
+        }
 
-      if (err) {
-        return callback(err);
-      }
-
-      pending = files.length;
-      if (!pending) {
-        return callback(null, list);
-      }
-
-      files.forEach(function (file) {
-        fs.stat(path + '/' + file, function (err, stats) {
-          if (err) {
-            return callback(err);
-          }
-
-          if (stats.isDirectory()) {
-            files = readdir(path + '/' + file, function (err, res) {
-              list = list.concat(res);
-              pending -= 1;
-              if (!pending) {
-                callback(null, list);
-              }
-            });
-          }
-          else {
-            list.push(path + '/' + file);
-            pending -= 1;
+        if (stats.isDirectory()) {
+          files = readdir(path + '/' + file, function (err, res) {
+            list = list.concat(res)
+            pending -= 1
             if (!pending) {
-              callback(null, list);
+              callback(null, list)
             }
+          })
+        }
+        else {
+          list.push(path + '/' + file)
+          pending -= 1
+          if (!pending) {
+            callback(null, list)
           }
-        });
-      });
-    });
-  }
+        }
+      })
+    })
+  })
+}
 
-
-  module.exports = readdir;
-}());
+module.exports = readdir
--- test/recursive-raddir-test.js
+++ test/recursive-raddir-test.js
@@ -1,27 +1,17 @@
-(function () {
-  "use strict";
-  var vows = require('vows')
-    , assert = require('assert')
-    , readdir = require(__dirname + '/../index')
-    ;
+var assert = require('assert')
+var readdir = require('../index')
 
-  vows.describe('readdir').addBatch({
-    'it works': {
-      topic: function () {
-        console.log(__dirname + '/testdir');
-        readdir(__dirname + '/testdir', this.callback);
-      },
-      'it correctly lists all files in nested directories': function (err, list) {
-        var expectedFiles
-          ;
+describe('readdir', function() {
+  it('correctly lists all files in nested directories', function (done) {
+    var expectedFiles = [__dirname + '/testdir/a/a', __dirname + '/testdir/a/beans',
+      __dirname + '/testdir/b/123', __dirname + '/testdir/b/b/hurp-durp',
+      __dirname + '/testdir/c.txt', __dirname + '/testdir/d.txt'
+    ]
 
-        expectedFiles = [__dirname + '/testdir/a/a', __dirname + '/testdir/a/beans',
-          __dirname + '/testdir/b/123', __dirname + '/testdir/b/b/hurp-durp',
-          __dirname + '/testdir/c.txt', __dirname + '/testdir/d.txt'
-        ].sort();
-        assert.strictEqual(null, err);
-        assert.deepEqual(list.sort(), expectedFiles.sort());
-      }
-    }
-  }).export(module);
-}());
+    readdir(__dirname + '/testdir', function(err, list) {
+      assert.ifError(err);
+      assert.deepEqual(list.sort(), expectedFiles.sort());
+      done()
+    })
+  })
+})
