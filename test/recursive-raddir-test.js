cf7b677c693be9b195729818b48dc39a412b96a5
ferhat elmas <elmas.ferhat@gmail.com>
2015-04-15 17:54:54 +0200

    utilized of path.join instead of concat

--- test/recursive-raddir-test.js
+++ test/recursive-raddir-test.js
@@ -1,14 +1,22 @@
 var assert = require('assert')
+var p = require('path')
 var readdir = require('../index')
+
+function getAbsolutePaths(files) {
+  return files.map(function(file) {
+    return p.join(__dirname, file)
+  })
+}
 
 describe('readdir', function() {
   it('correctly lists all files in nested directories', function (done) {
-    var expectedFiles = [__dirname + '/testdir/a/a', __dirname + '/testdir/a/beans',
-      __dirname + '/testdir/b/123', __dirname + '/testdir/b/b/hurp-durp',
-      __dirname + '/testdir/c.txt', __dirname + '/testdir/d.txt'
-    ]
+    var expectedFiles = getAbsolutePaths([
+      '/testdir/a/a', '/testdir/a/beans',
+      '/testdir/b/123', '/testdir/b/b/hurp-durp',
+      '/testdir/c.txt', '/testdir/d.txt'
+    ])
 
-    readdir(__dirname + '/testdir', function(err, list) {
+    readdir(p.join(__dirname, '/testdir'), function(err, list) {
       assert.ifError(err);
       assert.deepEqual(list.sort(), expectedFiles.sort());
       done()
@@ -16,12 +24,11 @@ describe('readdir', function() {
   })
 
   it('ignores the files listed in the ignores array', function (done) {
-    var notExpectedFiles = [
-      __dirname + '/testdir/d.txt',
-      __dirname + '/testdir/a/beans'
-    ]
+    var notExpectedFiles = getAbsolutePaths([
+      '/testdir/d.txt', '/testdir/a/beans'
+    ])
 
-    readdir(__dirname + '/testdir', ['d.txt', 'beans'], function(err, list) {
+    readdir(p.join(__dirname + '/testdir'), ['d.txt', 'beans'], function(err, list) {
       assert.ifError(err);
       list.forEach(function(file) {
         assert.equal(notExpectedFiles.indexOf(file), -1,
@@ -32,12 +39,11 @@ describe('readdir', function() {
   })
 
   it('supports ignoring files with just basename globbing', function (done) {
-    var notExpectedFiles = [
-      __dirname + '/testdir/d.txt',
-      __dirname + '/testdir/a/beans'
-    ]
+    var notExpectedFiles = getAbsolutePaths([
+      '/testdir/d.txt', '/testdir/a/beans'
+    ])
 
-    readdir(__dirname + '/testdir', ['*.txt', 'beans'], function(err, list) {
+    readdir(p.join(__dirname, '/testdir'), ['*.txt', 'beans'], function(err, list) {
       assert.ifError(err);
       list.forEach(function(file) {
         assert.equal(notExpectedFiles.indexOf(file), -1,
@@ -48,14 +54,13 @@ describe('readdir', function() {
   })
 
   it('supports ignoring files with the globstar syntax', function (done) {
-    var notExpectedFiles = [
-      __dirname + '/testdir/d.txt',
-      __dirname + '/testdir/a/beans'
-    ]
+    var notExpectedFiles = getAbsolutePaths([
+      '/testdir/d.txt', '/testdir/a/beans'
+    ])
 
     var ignores = ['**/*.txt', '**/a/beans']
 
-    readdir(__dirname + '/testdir', ignores, function(err, list) {
+    readdir(p.join(__dirname, '/testdir'), ignores, function(err, list) {
       assert.ifError(err);
       list.forEach(function(file) {
         assert.equal(notExpectedFiles.indexOf(file), -1,
@@ -66,7 +71,7 @@ describe('readdir', function() {
   })
 
   it('works when there are no files to report except ignored files', function(done) {
-    readdir(__dirname + '/testdirBeta', ['ignore.txt'], function(err, list) {
+    readdir(p.join(__dirname, '/testdirBeta'), ['ignore.txt'], function(err, list) {
       assert.ifError(err);
       assert.equal(list.length, 0, 'expect to report 0 files')
       done()
@@ -74,12 +79,9 @@ describe('readdir', function() {
   })
 
   it('works when negated ignore list is given', function(done) {
-
-    var expectedFiles = [
-      __dirname + '/testdir/c.txt',
-      __dirname + '/testdir/d.txt',
-      __dirname + '/testdirBeta/ignore.txt'
-    ]
+    var expectedFiles = getAbsolutePaths([
+      '/testdir/c.txt', '/testdir/d.txt', '/testdirBeta/ignore.txt'
+    ])
 
     readdir(__dirname, ['!*.txt'], function(err, list) {
       assert.ifError(err);
