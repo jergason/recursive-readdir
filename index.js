2a65498a5f983fc40cb2ae64bc3d3b8f6fef494c
Sylvain Guiblain <sylvain.guiblain@gmail.com>
2017-12-31 14:39:33 +0100

    Update Index.js to ES2015
    General update to ES2015 syntax:
     - Remove ;
     - Change function() { to () => {
     - Change foreach to a traditional for (for performance purpose only)

--- index.js
+++ index.js
@@ -1,96 +1,92 @@
-var fs = require("fs");
-var p = require("path");
-var minimatch = require("minimatch");
+let fs = require('fs')
+,		p = require('path')
+,		minimatch = require('minimatch')
 
 function patternMatcher(pattern) {
-  return function(path, stats) {
-    var minimatcher = new minimatch.Minimatch(pattern, { matchBase: true });
-    return (!minimatcher.negate || stats.isFile()) && minimatcher.match(path);
-  };
+  return (path, stats) => {
+    let minimatcher = new minimatch.Minimatch(pattern, { matchBase: true })
+    return (!minimatcher.negate || stats.isFile()) && minimatcher.match(path)
+  }
 }
 
 function toMatcherFunction(ignoreEntry) {
-  if (typeof ignoreEntry == "function") {
-    return ignoreEntry;
-  } else {
-    return patternMatcher(ignoreEntry);
-  }
+  return (typeof ignoreEntry == 'function') ? ignoreEntry : patternMatcher(ignoreEntry)
 }
 
 function readdir(path, ignores, callback) {
-  if (typeof ignores == "function") {
-    callback = ignores;
-    ignores = [];
+  if (typeof ignores == 'function') {
+    callback = ignores
+    ignores = []
   }
 
   if (!callback) {
-    return new Promise(function(resolve, reject) {
-      readdir(path, ignores || [], function(err, data) {
+    return new Promise((resolve, reject) => {
+      readdir(path, ignores || [], (err, data) => {
         if (err) {
-          reject(err);
+          reject(err)
         } else {
-          resolve(data);
+          resolve(data)
         }
-      });
-    });
+      })
+    })
   }
 
-  ignores = ignores.map(toMatcherFunction);
+  ignores = ignores.map(toMatcherFunction)
 
-  var list = [];
+  let list = []
 
-  fs.readdir(path, function(err, files) {
+  fs.readdir(path, (err, files) => {
     if (err) {
-      return callback(err);
+      return callback(err)
     }
 
-    var pending = files.length;
+    let pending = files.length
     if (!pending) {
       // we are done, woop woop
-      return callback(null, list);
+      return callback(null, list)
     }
 
-    files.forEach(function(file) {
-      var filePath = p.join(path, file);
-      fs.stat(filePath, function(_err, stats) {
+    for(let i = 0; i < pending; i++) {
+      let filePath = p.join(path, files[i])
+      fs.stat(filePath, (_err, stats) => {
         if (_err) {
-          return callback(_err);
+          return callback(_err)
         }
 
         if (
-          ignores.some(function(matcher) {
-            return matcher(filePath, stats);
+          ignores.some(matcher => {
+            return matcher(filePath, stats)
           })
         ) {
-          pending -= 1;
+          pending -= 1
           if (!pending) {
-            return callback(null, list);
+            return callback(null, list)
           }
-          return null;
+          return null
         }
 
         if (stats.isDirectory()) {
-          readdir(filePath, ignores, function(__err, res) {
+          readdir(filePath, ignores, (__err, res) => {
             if (__err) {
-              return callback(__err);
+              return callback(__err)
             }
 
-            list = list.concat(res);
-            pending -= 1;
+            list = list.concat(res)
+            pending -= 1
             if (!pending) {
-              return callback(null, list);
+              return callback(null, list)
             }
-          });
+          })
         } else {
-          list.push(filePath);
-          pending -= 1;
+          list.push(filePath)
+          pending -= 1
           if (!pending) {
-            return callback(null, list);
+            return callback(null, list)
           }
         }
-      });
-    });
-  });
+      })
+    }
+  })
 }
 
-module.exports = readdir;
+module.exports = readdir
