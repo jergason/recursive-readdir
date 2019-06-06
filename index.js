594394e3d4f436c40e89a9eeff99a39bf4404da6
Jamison Dance <jergason@gmail.com>
2019-06-05 23:51:57 -0600

    refactor to use promises internally

--- index.js
+++ index.js
@@ -11,95 +11,118 @@ function patternMatcher(pattern) {
   };
 }
 
+function readdirP(path) {
+  return new Promise(function(resolve, reject) {
+    fs.readdir(path, function(err, files) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(files);
+    });
+  });
+}
+
+function statP(path) {
+  return new Promise(function(resolve, reject) {
+    fs.stat(path, function(err, stats) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(stats);
+    });
+  });
+}
+
 function toMatcherFunction(ignoreEntry) {
   return typeof ignoreEntry == "function"
     ? ignoreEntry
     : patternMatcher(ignoreEntry);
 }
 
-function readdir(path, ignores, callback) {
+function recursiveReaddirCallback(path, ignores, callback) {
   if (typeof ignores == "function") {
     callback = ignores;
     ignores = [];
   }
 
   if (!callback) {
-    return new Promise(function(resolve, reject) {
-      readdir(path, ignores || [], function(err, data) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
-    });
+    return recursiveReaddir(path, ignores);
+  } else {
+    return recursiveReaddir(path, ignores).then(
+      function(list) {
+        return callback(null, list);
+      },
+      function(err) {
+        return callback(err, null);
+      }
+    );
   }
+}
 
+function recursiveReaddir(path, ignores) {
+  if (!ignores) {
+    ignores = [];
+  }
   ignores = ignores.map(toMatcherFunction);
 
   var list = [];
 
-  fs.readdir(path, function(err, files) {
-    if (err) {
-      return callback(err);
-    }
-
+  return readdirP(path).then(function(files) {
     // keep a counter of how many files we need to process
     // so it's easy to check when we're done
     var pending = files.length;
     if (!pending) {
       // we've hit a base case: no files inside this directory
       // return back up
-      return callback(null, list);
+      return list;
     }
 
-    files.forEach(function(file) {
-      var filePath = p.join(path, file);
-      fs.stat(filePath, function(_err, stats) {
-        if (_err) {
-          return callback(_err);
-        }
-
-        if (
-          ignores.some(function(matcher) {
-            return matcher(filePath, stats);
+    // return a promise when all files inside me have been resolved
+    return Promise.all(files.map(processFile(path, ignores))).then(function(
+      foundFiles
+    ) {
+      // TODO: filter out the undefineds this I think
+      // TODO: fix this, concat them?
+      // wait no this is a list of results from whatever processFile is
+      // flatten this?
+      return list.concat(
+        [].concat.apply(
+          [],
+          foundFiles.filter(function(f) {
+            // filter out underfineds
+            return !!f;
           })
-        ) {
-          pending -= 1;
-          if (!pending) {
-            // we've hit a base case: we've processed all the files
-            // return from the recursion
-            return callback(null, list);
-          }
-          return null;
-        }
-
-        if (stats.isDirectory()) {
-          readdir(filePath, ignores, function(__err, res) {
-            if (__err) {
-              return callback(__err);
-            }
-
-            list = list.concat(res);
-            pending -= 1;
-            if (!pending) {
-              // we've hit a base case: we've processed all the files
-              // return from the recursion
-              return callback(null, list);
-            }
-          });
-        } else {
-          list.push(filePath);
-          pending -= 1;
-          if (!pending) {
-            // we've hit a base case: we've processed all the files
-            // return from the recursion
-            return callback(null, list);
-          }
-        }
-      });
+        )
+      );
     });
   });
 }
 
-module.exports = readdir;
+// let's partially apply this for a nicer API
+function processFile(path, ignores) {
+  return function(file) {
+    var filePath = p.join(path, file);
+    return statP(filePath).then(function(stats) {
+      // if we should ignore this file, skip over it
+      if (
+        ignores.some(function(matcher) {
+          return matcher(filePath, stats);
+        })
+      ) {
+        // if this file should be ignored, just skip over it
+        return;
+      }
+
+      if (stats.isDirectory()) {
+        // if it's a directory, recurse through it
+        return recursiveReaddir(filePath, ignores);
+      } else {
+        // just return the individual file
+        return filePath;
+      }
+    });
+  };
+}
+
+// we expose this as a the top-level API to maintain backwards compatability
+module.exports = recursiveReaddirCallback;
